fc99eca82b834a39a08422e5cdff6e7a0c3316e5
sumit <sumitrohit2004@gmail.com>
2024-03-13 00:31:49 +0530

    LOGIN AND REGISTER CREATED

--- App.jsx
+++ App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Text } from "react-native";
 import GoogleMapsScreen from "./src/screens/googlemaps";
-
+import LoginScreen from "./src/screens/googlemaps/login";
+import RegisterScreen from "./src/screens/googlemaps/Register";
 
 const App = ()=>{
     return (
-        <GoogleMapsScreen/>
+        // <GoogleMapsScreen/>
+        // <LoginScreen/>
+        <RegisterScreen/>
     )
 }
 
