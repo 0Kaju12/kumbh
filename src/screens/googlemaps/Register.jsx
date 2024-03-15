ac8fe1b627cf2c15b7127e18550e48cc0568dc00
sumit <sumitrohit2004@gmail.com>
2024-03-15 14:38:15 +0530

    Update and Send Latitude AND Longitude

--- src/screens/googlemaps/Register.jsx
+++ src/screens/googlemaps/Register.jsx
@@ -1,18 +1,44 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
-import {} from "@react-navigation/native-stack"
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from 'react-native';
 
-const RegisterScreen = ({ navigation }) => {
+const RegisterScreen = ({navigation}) => {
   const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [phone, setPhone]= useState('');
 
-  const handleLogin = () => {
-    // Here you can implement your login logic, for simplicity, I'm just logging the username and password
-    console.log('Username:', username);
-    console.log('Password:', password);
-    // Navigate to GoogleMapsScreen after successful login
-    navigation.navigate('Login');
+  const handleLogin = async () => {
+    console.log(email);
+    console.log(password);
+    try {
+      const response = await fetch('http://10.0.2.2:3000/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: username,
+          email: email,
+          password: password,
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+      if (data.message === 'User registered successfully') {
+        // If login is successful, navigate to GoogleMapsScreen
+        navigation.navigate('GoogleMaps');
+      } else {
+        // If login fails, handle error (e.g., display error message)
+        console.error('Register failed');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
   };
 
   return (
@@ -20,9 +46,20 @@ const RegisterScreen = ({ navigation }) => {
       <Text style={styles.title}>Register</Text>
       <TextInput
         style={styles.input}
-        placeholder="Username"
+        placeholder="username"
         onChangeText={text => setUsername(text)}
         value={username}
+        keyboardType="default"
+        autoCapitalize="none"
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        onChangeText={text => setEmail(text)}
+        value={email}
+        keyboardType="email-address"
+        autoCapitalize="none"
       />
       <TextInput
         style={styles.input}
@@ -31,15 +68,14 @@ const RegisterScreen = ({ navigation }) => {
         value={password}
         secureTextEntry
       />
-      <TextInput
-        style={styles.input}
-        placeholder="Phone No"
-        onChangeText={text => setPhone(text)}
-        value={phone}
-        secureTextEntry
-      />
+
       <TouchableOpacity style={styles.button} onPress={handleLogin}>
         <Text style={styles.buttonText}>Register</Text>
+      </TouchableOpacity>
+
+      {/* Add a link to navigate to the Register screen */}
+      <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+        <Text style={styles.linkText}>Already have an account? Login here</Text>
       </TouchableOpacity>
     </View>
   );
@@ -71,11 +107,16 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     paddingHorizontal: 80,
     borderRadius: 5,
+    marginBottom: 10, // Add marginBottom to separate the button and link
   },
   buttonText: {
     color: 'white',
     fontWeight: 'bold',
     fontSize: 16,
+  },
+  linkText: {
+    color: 'blue',
+    textDecorationLine: 'underline',
   },
 });
 
--- src/screens/googlemaps/index.jsx
+++ src/screens/googlemaps/index.jsx
@@ -1,5 +1,5 @@
-import { View, Text, StyleSheet, Image, Platform, PermissionsAndroid } from 'react-native';
 import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Image, Platform, PermissionsAndroid } from 'react-native';
 import MapView, { Marker, PROVIDER_GOOGLE, Circle } from 'react-native-maps';
 import Green_Marker from '../../assets/Green_Marker.png';
 import GetLocation from 'react-native-get-location';
@@ -19,14 +19,61 @@ const styles = StyleSheet.create({
 function GoogleMapsScreen() {
   const [permission, setPermission] = useState(false);
   const [marker, setMarker] = useState({
-    key: 1,
-    latitude: 25.429720,
+    latitude: 25.42972,
     longitude: 81.771385,
     name: "Kush",
     description: "All good"
   });
 
+  const _saveLocation = async (lat, long) => {
+    try {
+      const response = await fetch('http://10.0.2.2:3000/saveLocation', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          userId: "65f35676834dd4fcda7a4bb8",
+          lat: lat.toString(),
+          long: long.toString()
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+      if (data.message === 'Location registered successfully') {
+        console.log('Location saved successfully');
+      } else {
+        console.error('Failed to save location');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
+  const _updateLocation = async (lat, long) => {
+    try {
+      const response = await fetch('http://10.0.2.2:3000/updateLocation', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          userId: "65f35676834dd4fcda7a4bb8",
+          lat: lat.toString(),
+          long: long.toString()
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+      if (data.message === 'Location updated successfully') {
+        console.log('Location Updated successfully');
+      } else {
+        console.error('Failed to Update location');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
   const _getLocationPermission = async () => {
     if (Platform.OS === "android") {
@@ -40,11 +87,11 @@ function GoogleMapsScreen() {
             buttonNegative: 'Cancel',
             buttonPositive: 'OK',
           }
-      
         );
         if (granted === PermissionsAndroid.RESULTS.GRANTED) {
           setPermission(true);
-          _getCurrentLocation();
+          await _getCurrentLocation();
+          _saveLocation(marker.latitude,marker.longitude)
           console.log('You can use the app');
         } else {
           console.log('Location permission denied');
@@ -54,14 +101,11 @@ function GoogleMapsScreen() {
       }
     }
   };
-  useEffect(() => {
-    _getLocationPermission();
-  }, []);
 
   const _getCurrentLocation = () => {
     GetLocation.getCurrentPosition({
       enableHighAccuracy: true,
-      timeout: 60000,
+      timeout: 10000,
     })
       .then(location => {
         console.log("My current location =>", location);
@@ -70,49 +114,61 @@ function GoogleMapsScreen() {
           latitude: location.latitude,
           longitude: location.longitude
         });
+        _updateLocation(location.latitude, location.longitude);
+        // Save location when page is rendered
+        
       })
       .catch(error => {
         const { code, message } = error;
         console.warn(code, message);
       });
   };
-  if (!permission) {
-    return (
-      <View style={styles.container}>
-        <Text>Please allow location permission to continue...</Text>
-      </View>
-    );
-  }
 
+  useEffect(() => {
+    _getLocationPermission();
+  }, []);
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      _getCurrentLocation();
+    }, 20000);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <View style={styles.container}>
-      <MapView
-        provider={PROVIDER_GOOGLE}
-        style={styles.map}
-        region={{
-          latitude: marker.latitude,
-          longitude: marker.longitude,
-          latitudeDelta: 0.015,
-          longitudeDelta: 0.0121,
-        }}>
-        <Marker
-          coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
-          title={marker.name}
-          description={marker.description}>
-          <Image style={{ width: 60, height: 60, opacity: 0.7 }} source={Green_Marker} />
-        </Marker>
-        <Circle
-          center={{
+      {permission ? (
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          region={{
             latitude: marker.latitude,
-            longitude: marker.longitude
+            longitude: marker.longitude,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
           }}
-          radius={100}
-          strokeColor='black'
-          fillColor='#EBF5FB'
-        />
-      </MapView>
+        >
+          <Marker
+            coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
+            title={marker.name}
+            description={marker.description}
+          >
+            <Image style={{ width: 60, height: 60, opacity: 0.7 }} source={Green_Marker} />
+          </Marker>
+          <Circle
+            center={{
+              latitude: marker.latitude,
+              longitude: marker.longitude
+            }}
+            radius={100}
+            strokeColor='black'
+            fillColor='#EBF5FB'
+          />
+        </MapView>
+      ) : (
+        <Text>Please allow location permission to continue...</Text>
+      )}
     </View>
   );
 }
